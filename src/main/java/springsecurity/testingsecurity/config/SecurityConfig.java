cf827b9bdd8aa4f4f56927f549d6f2f1ce24d2ec
dat2503 <dat.nc2503@gmail.com>
2023-03-20 23:22:22 +0700

    implementing remember me

--- src/main/java/springsecurity/testingsecurity/config/SecurityConfig.java
+++ src/main/java/springsecurity/testingsecurity/config/SecurityConfig.java
@@ -2,9 +2,14 @@ package springsecurity.testingsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,10 +24,45 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http.csrf().and().cors().disable()
                 .authorizeHttpRequests()
-                .antMatchers("/home").permitAll()
-                .antMatchers("/student")
+                .antMatchers("/").permitAll()
+                .anyRequest()
                 .authenticated()
+
+                .and()
+                .formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/")
+                .failureUrl("/login.html?error=true")
+
+                .and()
+                .logout()
+                .deleteCookies("JSESSIONID")
+
+                .and()
+                .rememberMe()
+                .key("uniqueAndSecret")
+
                 .and()
                 .build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception{
+        return http.getSharedObject(AuthenticationManagerBuilder.class).build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        UserDetails user = User.withUsername("user1")
+                .password("password")
+                .authorities("ROLE_USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("admin")
+                .authorities("ROLE_ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
 }
